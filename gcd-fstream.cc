795ef7663cfd7509d4a902aaa8e2430a6a5c3b4d
Alison Chaiken <alison@she-devel.com>
2017-08-05 12:49:15 -0700

    change gcd-fstream to strtoul

--- gcd-fstream.cc
+++ gcd-fstream.cc
@@ -7,35 +7,37 @@
 
 using namespace std;
 
-int get_op(ifstream &ifile) {
+unsigned long get_op(ifstream &ifile) {
   char instring[32];
-  int op = 0;
+  unsigned long op = 0;
 
   // use of get() instead of getline() puts terminating
-  // char into returned string and causes strtoul() to fail.
+  // into returned string and causes strtoul() to fail.
   // With get(), if the terminating character is ' ', then the second
   // string read will have only ' '.
   ifile.getline(instring, 32, ' ');
   if (!strlen(instring)) {
     exit(EXIT_SUCCESS);
   }
-  //  op = strtoul(instring, nullptr, 10);
-  op = atoi(instring);
-  if ((errno == ERANGE) || (op == 0)) {
+  op = strtoul(instring, nullptr, 10);
+  // atoi() is considered deprecated, will be replaced
+  // by strtoi().
+  // op = atoi(instring);
+  if ((errno == ERANGE) || (errno == EINVAL) || (op == 0)) {
     cerr << "Operand out of range: " << op << endl;
     exit(EXIT_FAILURE);
   }
   return op;
 }
 
-int gcd(int a, int b) {
+unsigned long gcd(unsigned long a, unsigned long b) {
   if ((a * b) == 0) {
     cerr << "Can't process zero." << endl;
     exit(EXIT_FAILURE);
   }
-  int divisor = (a < b) ? a : b;
-  int dividend = (a < b) ? b : a;
-  int remainder = divisor;
+  unsigned long divisor = (a < b) ? a : b;
+  unsigned long dividend = (a < b) ? b : a;
+  unsigned long remainder = divisor;
 
   while (remainder != 0) {
     remainder = dividend % divisor;
@@ -68,9 +70,9 @@ int main(int argc, char **argv) {
     cerr << "Couldn't open requested file:" << argv[2] << endl;
     exit(EXIT_FAILURE);
   }
-  int num1 = 0, num2 = 0;
+  unsigned long num1 = 0, num2 = 0;
   while ((num1 = get_op(ifile)) && (num2 = get_op(ifile))) {
-    int ans = gcd(num1, num2);
+    unsigned long ans = gcd(num1, num2);
     ofile << "gcd of ";
     ofile << num1;
     ofile << ", ";
@@ -86,3 +88,22 @@ int main(int argc, char **argv) {
   ifile.close();
   ofile.close();
 }
+///////////////////////////////////////////////////////////////////////////////
+//									     //
+//	[alison@bonnet Pohl (master)]$ ./gcd-fstream foo bar		     //
+//	[alison@bonnet Pohl (master)]$ echo $?				     //
+//	0								     //
+//	[alison@bonnet Pohl (master)]$ cat foo				     //
+//	100 35 11 99 256 1000 3 1449 31a 3b				     //
+//	[alison@bonnet Pohl (master)]$ cat bar				     //
+//	gcd of 100, 35 is 5						     //
+//	file position is 20						     //
+//	gcd of 11, 99 is 11						     //
+//	file position is 60						     //
+//	gcd of 256, 1000 is 8						     //
+//	file position is 102						     //
+//	gcd of 3, 1449 is 3						     //
+//	file position is 143						     //
+//	gcd of 31, 3 is 1						     //
+//	file position is 182						     //
+///////////////////////////////////////////////////////////////////////////////
