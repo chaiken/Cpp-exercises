733d581b6078ba22044ea83426e7c945c3af967a
Alison Chaiken <alison@she-devel.com>
2018-05-07 06:57:55 -0700

    notqsort: simply function logic

--- notqsort_lib.cc
+++ notqsort_lib.cc
@@ -27,28 +27,33 @@ void print_values(int *base, unsigned start, unsigned num) {
   cout << "}" << endl;
 }
 
+// We can make base a void* and pass size_t size into it,
+// but base += size is illegal without first casting base
+// to a type.  Not only compare() but print_values() would
+// need to be passed in as a different function pointer for
+// each type.  The right way to solve this problem in C++ is
+// with a template.
 void notqsort(int *base, size_t num, size_t len,
               int func(const int *, const int *)) {
   assert(base != 0);
-  assert(num > 0u);
+  if (num < 2u) {
+    return;
+  }
   // unsigned depth = len - num;
   // cout << " depth: " << len - num << endl;
   // print_values(base, depth, len);
   // cout << endl;
 
-  // Sort the first pair.  This is the step that does work.
-  if (func(base, base + 1) < 0) {
-    swaptwo(base, base + 1);
-  }
-  // If unsorted material remains at the end, continue.
   // Simplification occurs by comparing fewer elements.
-  if (num > 2) {
+  // Process last two elements, then last 3 . . .
+  if (num > 2u) {
     notqsort(base + 1, num - 1, len, func);
   }
-  // The values near the beginning of the list may need to be sorted a second
-  // time.
   if (func(base, base + 1) < 0) {
+    // Sort the pair at the cursor.  This is the step that does all the work.
     swaptwo(base, base + 1);
+    // May have to resort the end of the array based on changes at the
+    // beginning.
     notqsort(base + 1, num - 1, len, func);
   }
   return;
