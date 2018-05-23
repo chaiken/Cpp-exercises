a54784b0001faac5e355cd06d4f82b0c5a6412fe
Alison Chaiken <alison@she-devel.com>
2018-05-23 07:31:40 -0700

    safe vector dbl_vector: parameterized test SetUp()

--- dbl_vector_lib_test.cc
+++ dbl_vector_lib_test.cc
@@ -1,15 +1,31 @@
 #include "dbl_vector.h"
 #include "gtest/gtest.h"
 
+// From Setup(), I can only call the default constructor from
+// not the parameterized ones.
+// https://stackoverflow.com/questions/38207346/specify-constructor-arguments-for-a-google-test-fixture
+//  you cannot achieve what you want by instantiating a fixture using a
+//  non-default constructor. However, there are other ways. Simply overload
+//  the SetUp function and call that version explicitly in the tests.
+
 using namespace std;
 using namespace dbl_vect;
 
-TEST(DoubleVectorTest, SumElementsWorks) {
-  DoubleVector a(5);
-  int i = 0;
-  while (i <= a.ub()) {
-    a.element(i) = 2.0 * i;
-    i++;
+class DoubleVectorTest : public ::testing::Test {
+public:
+  void SetUp(int n) {
+    a = new DoubleVector(n);
+    int i = 0;
+    while (i <= a->ub()) {
+      a->element(i) = 2.0 * i;
+      i++;
+    }
   }
-  ASSERT_EQ(20.0, a.SumElements());
+  virtual void TearDown() { delete a; }
+  DoubleVector *a;
+};
+
+TEST_F(DoubleVectorTest, SumElementsWorks) {
+  SetUp(5);
+  ASSERT_EQ(20.0, a->SumElements());
 }
