4c1be386d83ec5a081acd6a230f4a7e298cb064b
Alison Chaiken <alison@she-devel.com>
2022-04-16 09:51:38 -0700

    one_index_vector: make sure moved-from vectors are empty

--- one_index_vector_impl.h
+++ one_index_vector_impl.h
@@ -4,12 +4,14 @@ namespace one_index_vector {
 template <typename T> OneIndexVector<T>::OneIndexVector(std::vector<T> &&vec) {
   ::std::cout << "local move constructor" << ::std::endl;
   std::swap(*this, vec);
+  vec.erase(vec.begin(), vec.end());
 }
 
 template <typename T>
 OneIndexVector<T> &OneIndexVector<T>::operator=(std::vector<T> &&vec) {
   std::cout << "local move assignment" << std::endl;
   std::swap(*this, vec);
+  vec.erase(vec.begin(), vec.end());
   return *this;
 }
 
