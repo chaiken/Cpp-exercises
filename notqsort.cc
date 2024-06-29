f2fd79217889795f81daf91131ffc5b313a73249
Alison Chaiken <alison@she-devel.com>
2024-06-29 10:48:51 -0700

    notqsort: fix ambiguous variable name
    Apparently the STL's range_access has a variable called data.

--- notqsort.cc
+++ notqsort.cc
@@ -7,17 +7,17 @@
 using namespace std;
 using namespace not_qsort;
 
-int data[] = {9, 4, 23, 43, 99, 4955, -1, 234};
-constexpr size_t kHowmany = sizeof(data) / sizeof(int);
+int input[] = {9, 4, 23, 43, 99, 4955, -1, 234};
+constexpr size_t kHowmany = sizeof(input) / sizeof(int);
 
 int main(void) {
 
-  notqsort(data, kHowmany, kHowmany, compare);
-  print_values(data, 0u, kHowmany);
+  notqsort(input, kHowmany, kHowmany, compare);
+  print_values(input, 0u, kHowmany);
   cout << endl;
 
-  notqsort(data, kHowmany, kHowmany, revcompare);
-  print_values(data, 0u, kHowmany);
+  notqsort(input, kHowmany, kHowmany, revcompare);
+  print_values(input, 0u, kHowmany);
   cout << endl;
 
   exit(EXIT_SUCCESS);
